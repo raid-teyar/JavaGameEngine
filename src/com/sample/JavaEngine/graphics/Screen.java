1b0810f1ddada42c1642f012167f4f8dfac6d14c
Raid Teyar <63502859+raid-teyar@users.noreply.github.com>
2021-12-21 16:16:42 +0100

    Added Tiles system

--- src/com/sample/JavaEngine/graphics/Screen.java
+++ src/com/sample/JavaEngine/graphics/Screen.java
@@ -1,13 +1,23 @@
 package com.sample.JavaEngine.graphics;
 
+import java.util.Random;
+
 public class Screen {
     private int width, height;
     public int[] pixels;
+    public final int MAP_SIZE = 64;
+    public final int MAP_SIZE_MASK = MAP_SIZE - 1;
+
+    public int[] tiles = new int[MAP_SIZE * MAP_SIZE];
+    private Random random = new Random();
 
     public Screen(int width, int height) {
         this.width = width;
         this.height = height;
         pixels = new int[width * height];
+        for (int i = 0; i < MAP_SIZE * MAP_SIZE; i++){
+            tiles[i] = random.nextInt(0xffffff);
+        }
     }
 
     public void clear() {
@@ -18,10 +28,13 @@ public class Screen {
 
     public void render(){
         for (int y = 0; y < height; y++){
-            if (y >= height || y < 0) break;
+            int yy = y;
+            //if ( yy < 0 || yy >= height ) break;
             for (int x = 0; x < width; x++){
-                if (x >= height || x < 0) break;
-                pixels[x + y * width] = 0xff00ff;
+                int xx = x;
+                //if (xx < 0 || xx >= width) break;
+                int tileIndex = ((xx >> 4) & MAP_SIZE_MASK) + ((yy >> 4) & MAP_SIZE_MASK) * MAP_SIZE;
+                pixels[x + y * width] = tiles[tileIndex];
             }
         }
     }
